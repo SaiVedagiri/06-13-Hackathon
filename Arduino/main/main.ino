da5cbd3034258b0491cf727c0334bc81ea0036db
Arya Tschand <43081871+aryatschand@users.noreply.github.com>
2020-06-14 02:56:13 -0400

    final arduino code for RFID

--- Arduino/main/main.ino
+++ Arduino/main/main.ino
@@ -11,8 +11,6 @@
 
 #include "WiFiEsp.h"
 
-#include <Servo.h>
-
 // Emulate MySerial on pins 6/7 if not present
 #ifndef HAVE_HWSERIAL1
 #include "SoftwareSerial.h"
@@ -20,7 +18,7 @@
 SoftwareSerial Serial1(18, 19); // RX, TX
 #endif
 
-#define echoPin 41 // attach pin D2 Arduino to pin Echo of HC-SR04
+#define echoPin 7 // attach pin D2 Arduino to pin Echo of HC-SR04
 #define trigPin 39 //attach pin D3 Arduino to pin Trig of HC-SR04
 
 // defines variables
@@ -33,8 +31,8 @@ char pass[] = "parwan123";        // your network password
 int status = WL_IDLE_STATUS;     // the Wifi radio's status
 float oldvolume = 0;
 
-String url = "reserveid.macrotechsolutions.us";
-char server[] = "reserveid.macrotechsolutions.us";
+String url = "safetravels.macrotechsolutions.us";
+char server[] = "safetravels.macrotechsolutions.us";
 
 // twelve servo objects can be created on most boards
 #define SDA_DIO 9
@@ -44,8 +42,6 @@ RFID RC522(SDA_DIO, RESET_DIO);
 
 // Initialize the Ethernet client object
 WiFiEspClient client;
-
-Servo myservo;
 
 void setup()
 {
@@ -57,8 +53,6 @@ void setup()
 
   pinMode(trigPin, OUTPUT); // Sets the trigPin as an OUTPUT
   pinMode(echoPin, INPUT); // Sets the echoPin as an INPUT
-
-  myservo.attach(33);
 
   SPI.begin();
   /* Initialise the RFID reader */
@@ -95,58 +89,26 @@ void loop()
 {
   while (!RC522.isCard())
   {
-    makeColorCall();
-    //while (client.connected() || client.available())
-    String result = "";
-    for (int x = 0; x < 10; x++)
-    {
-      if (client.available())
-      {
-        String line = client.readStringUntil("}");
-        Serial.println(line);
-        String newline = line.substring(line.length() - 3, line.length() - 2);
-        if (newline == "w") {
+      delay(50);
 
-
-          digitalWrite(25, HIGH);
-          digitalWrite(29, LOW);
-          digitalWrite(23, LOW);
-
-        } else if (newline == "d") {
-          digitalWrite(23, HIGH);
-          digitalWrite(29, LOW);
-          digitalWrite(25, LOW);
-        } else {
-          digitalWrite(25, HIGH);
-          digitalWrite(29, LOW);
-          digitalWrite(23, LOW);
-        }
+      digitalWrite(trigPin, LOW);
+      delayMicroseconds(2);
+      // Sets the trigPin HIGH (ACTIVE) for 10 microseconds
+      digitalWrite(trigPin, HIGH);
+      delayMicroseconds(10);
+      digitalWrite(trigPin, LOW);
+      // Reads the echoPin, returns the sound wave travel time in microseconds
+      duration = pulseIn(echoPin, HIGH);
+      // Calculating the distance
+      distance = duration * 0.034 / 2; // Speed of sound wave divided by 2 (go and back)
+      if (olddistance > distance + 5 or olddistance < distance - 5) {
+        olddistance = distance;
       }
-      delay(1);
-    }
-    delay(100);
-    client.stop();
-    delay(500);
-
-    digitalWrite(trigPin, LOW);
-    delayMicroseconds(2);
-    // Sets the trigPin HIGH (ACTIVE) for 10 microseconds
-    digitalWrite(trigPin, HIGH);
-    delayMicroseconds(10);
-    digitalWrite(trigPin, LOW);
-    // Reads the echoPin, returns the sound wave travel time in microseconds
-    duration = pulseIn(echoPin, HIGH);
-    // Calculating the distance
-    distance = duration * 0.034 / 2; // Speed of sound wave divided by 2 (go and back)
-    if (olddistance > distance + 5 or olddistance < distance - 5) {
-      olddistance = distance;
-      distanceCall(String(distance));
-      client.stop();
-    }
-    delay(500);
-
+      delay(500);
+      digitalWrite(29, HIGH);
+      digitalWrite(25, LOW);
+      digitalWrite(23, LOW);
   }
-  Serial.println("scanned");
   /* If so then get its serial number */
   RC522.readCardSerial();
   String key = "";
@@ -155,16 +117,11 @@ void loop()
     key += String(RC522.serNum[i], DEC);
     //Serial.print(RC522.serNum[i],HEX); //to print card detail in Hexa Decimal format
   }
+  Serial.println("Scanned RFID " + key);
   makeCall(key);
-  digitalWrite(29, HIGH);
-          digitalWrite(25, LOW);
-          digitalWrite(23, LOW);
-  for (int pos = 0; pos <= 180; pos += 1) // goes from 0 degrees to 180 degrees
-        { // in steps of 1 degree
-          myservo.write(pos);              // tell servo to go to position in variable 'pos'
-          delay(15);                       // waits 15ms for the servo to reach the position
-        }
-        
+  digitalWrite(29, LOW);
+  digitalWrite(25, HIGH);
+  digitalWrite(23, LOW);
   
   delay(50);
   //while (client.connected() || client.available())
@@ -174,17 +131,10 @@ void loop()
     if (client.available())
     {
       String line = client.readStringUntil('}');
-      String newline = line.substring(line.length() - 2, line.length() - 1);
-      if (newline == "y") {
-        for (int pos = 0; pos <= 180; pos += 1) // goes from 0 degrees to 180 degrees
-        { // in steps of 1 degree
-          myservo.write(pos);              // tell servo to go to position in variable 'pos'
-          delay(15);                       // waits 15ms for the servo to reach the position
-        }
-      }
     }
     delay(1);
   }
+  Serial.println("HTTP Success");
   delay(100);
   client.stop();
   delay(500);
@@ -194,27 +144,8 @@ void makeCall(String key) {
   if (client.connect(server, 80)) {
     Serial.println("Connected to server");
     // Make a HTTP request
-    client.println("GET /hardwareConnect?rfid=" + key + " HTTP/1.1");
-    client.println("Host: " + url);
-    client.println();
-  }
-}
-
-void makeColorCall() {
-  if (client.connect(server, 80)) {
-    Serial.println("Connected to server");
-    // Make a HTTP request
-    client.println("GET /getColor HTTP/1.1");
-    client.println("Host: " + url);
-    client.println();
-  }
-}
-
-void distanceCall(String key)   {
-  if (client.connect(server, 80)) {
-    Serial.println("Connected to server");
-    // Make a HTTP request
-    client.println("GET /distance?dist=" + key + " HTTP/1.1");
+    //Serial.println("/hardwareConnect?rfid=" + key + "&busid=-M9ibFAVEKjdDQIch3hB&type=bus&position=2,4&timeid=-M9ibFAa46Xsm3OkwHnC");
+    client.println("GET /hardwareConnect?rfid=" + key + "&vehicleid=-M9ibFAVEKjdDQIch3hB&type=bus&position=2,4&timeid=-M9ibFAa46Xsm3OkwHnC HTTP/1.1");
     client.println("Host: " + url);
     client.println();
   }
